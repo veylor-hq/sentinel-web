c45fe3fdb6e5cae0f051a1fdc349371b4c44ba0c
Ihor Savenko <csigorek@gmail.com>
2025-09-16 20:27:02 +0100

    temo remove checks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,11 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      "*",
     ],
   },
 ];
